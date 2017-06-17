5a771bbc30b2c98762ccb5f2e0947e09035f09b4
JohnYunhan <18702516203@126.com>
2017-06-17 21:46:43 +0800

    完成推荐页的上拉加载更多，下拉刷新

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,5 +1,6 @@
 import { commonParams, options } from './config';
 import jsonp from 'common/js/jsonp';
+import axios from 'axios';
 
 export function getRecommend() {
   let url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
@@ -9,4 +10,29 @@ export function getRecommend() {
     needNewCode: 1
   });
   return jsonp(url, data, options);
+};
+
+export function getSongList(start, end) {
+  let url = 'api/getSongList';
+  let data = {
+    rnd: Math.random(),
+    g_tk: 5381,
+    loginUin: 0,
+    hostUin: 0,
+    format: 'json',
+    inCharset: 'utf8',
+    outCharset: 'utf-8',
+    notice: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    categoryId: 10000000,
+    sortId: 5,
+    sin: start,
+    ein: end
+  };
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data);
+  });
 }
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="recommend" ref="scrollRecommend">
     <div class="recommend-content">
-      <div class="img-swiper">
+      <div class="top-tip" :style="{'top':showTopTip?'0':'-60px'}">{{topTip}}</div>
+      <div class="img-swiper" :style="{'margin-top':showTopTip?'60px':'0'}">
         <swiper :options="swiperOption">
           <swiper-slide v-for="item in recommends" :key="item.id">
             <a :href="item.linkUrl"><img :src="item.picUrl" width="100%"></a>
@@ -12,10 +13,10 @@
       <div class="radio-wrapper">
         <h1 class="title">热门电台</h1>
         <ul class="radio-list">
-          <li v-for="list in radioList" :key="list.radioid" class="radio-item">
+          <li v-for="list in radioList" :key="list.radioid" class="radio-item" v-ripple="'rgba(255, 255, 255, 0.5)'">
             <a :href="list.linkUrl">
               <div class="radio-img">
-                <img :src="list.picUrl">
+                <img v-lazy="list.picUrl">
               </div>
               <h2 class="name">{{list.Ftitle}}</h2>
             </a>
@@ -24,23 +25,25 @@
       </div>
       <div class="hot-song-list">
         <h1 class="title">热门歌单</h1>
-        <div class="song-list" v-for="song in songList" :key="song.id">
+        <div class="song-list" v-for="song in songList" :key="song.id" v-ripple>
           <div class="icon">
-            <img :src="song.picUrl" width="60" @load="loadImage">
+            <img v-lazy="song.imgurl" width="60">
           </div>
           <div class="text">
-            <div class="name">{{song.songListAuthor}}</div>
-            <p class="desc">{{song.songListDesc}}</p>
+            <div class="name" v-html="song.creator.name"></div>
+            <p class="desc" v-html="song.dissname"></p>
           </div>
         </div>
       </div>
+      <div class="bottom-tip">{{bottomTip}}</div>
     </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
 import Vue from 'vue';
 import {
-  getRecommend
+  getRecommend,
+  getSongList
 } from 'api/recommend';
 import {
   ERR_OK
@@ -63,11 +66,18 @@ export default {
         },
         radioList: [],
         songList: [],
-        radioUrl: ['https://c.y.qq.com/v8/playsong.html?ADTAG=myqq&from=myqq&channel=10007100&songid=5452502,11772,101525,89756,1425425,416764,1007827,237281,7228564,5408217,855611,102679605,3596012,102338859,105161539,7012266,137873,7702,102371771,4829977', 'https://c.y.qq.com/v8/playsong.html?ADTAG=myqq&from=myqq&channel=10007100&songid=109839552,202497114,200506302,202325928,201712321,104919028,104244290,105460886,102806932,105664042,202049046,109671452,200374390,202177901,105533676,109472611,202116648,200904285,200535850,102419875']
+        radioUrl: ['https://c.y.qq.com/v8/playsong.html?ADTAG=myqq&from=myqq&channel=10007100&songid=5452502,11772,101525,89756,1425425,416764,1007827,237281,7228564,5408217,855611,102679605,3596012,102338859,105161539,7012266,137873,7702,102371771,4829977', 'https://c.y.qq.com/v8/playsong.html?ADTAG=myqq&from=myqq&channel=10007100&songid=109839552,202497114,200506302,202325928,201712321,104919028,104244290,105460886,102806932,105664042,202049046,109671452,200374390,202177901,105533676,109472611,202116648,200904285,200535850,102419875'],
+        start: 0,
+        end: 29,
+        bottomTip: '查看更多',
+        topTip: '下拉刷新',
+        loadMore: true,
+        showTopTip: false
       };
     },
     created() {
       this._getRecommend();
+      this._getSongList(this.start, this.end);
     },
     methods: {
       _getRecommend() {
@@ -78,27 +88,102 @@ export default {
             for (var i = 0; i < this.radioUrl.length; i++) {
               Vue.set(this.radioList[i], 'linkUrl', this.radioUrl[i]);
             }
-            this.songList = res.data.songList;
-            this.$nextTick(() => {
-              this._initScroll();
-            });
+            this._initScroll();
+          }
+        });
+      },
+      _getSongList(start, end) {
+        getSongList(start, end).then((res) => {
+          if (res.code === ERR_OK) {
+            // 最多获取300条数据
+            if (start >= 300) {
+              this.bottomTip = '没有更多数据了…';
+              this.loadMore = false;
+            } else {
+              this.bottomTip = '查看更多';
+              this.loadMore = true;
+            }
+            this.songList = this.songList.concat(res.data.list);
+            setTimeout(() => {
+              this.scroll.refresh();
+              this.topTip = '刷新成功';
+              console.log(this.topTip);
+            }, 1000);
+            setTimeout(() => {
+              this.showTopTip = false;
+              this.topTip = '下拉刷新';
+              console.log(this.topTip);
+            }, 5000);
           }
         });
       },
       _initScroll() {
+        var _this = this;
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.scrollRecommend, {
-            click: true
+            click: true,
+            probeType: 1
           });
         } else {
           this.scroll.refresh();
         }
+        this.scroll.on('touchend', function(pos) {
+          // 上拉加载
+          if (pos.y < (this.maxScrollY - 50) && _this.loadMore) {
+            _this.bottomTip = '加载中…';
+            setTimeout(() => {
+              _this.reloadData();
+            }, 1000);
+          }
+          // 下拉刷新
+          if (pos.y > 50) {
+            console.log(pos.y, 2);
+            _this.topTip = '正在刷新…';
+            _this.showTopTip = true;
+            setTimeout(() => {
+              _this.start = 0;
+              _this.end = 29;
+              _this.bottomTip = '';
+              _this.songList = [];
+              _this._getRecommend();
+              _this._getSongList(_this.start, _this.end);
+            }, 1000);
+          }
+        });
+        this.scroll.on('scroll', function(pos) {
+          console.log(pos.y, 1);
+          if (pos.y > 50) {
+            _this.topTip = '释放立即刷新';
+          } else {
+            _this.topTip = '下拉刷新';
+          }
+        });
+      },
+      reloadData() {
+        this.start += 30;
+        this.end += 30;
+        this._getSongList(this.start, this.end);
       },
       loadImage() {
         if (!this.checkloaded) {
           this.checkloaded = true;
           this.scroll.refresh();
         }
+      }
+    },
+    filters: {
+      handlerData: function(val) {
+        val = val.replace(/&#32;/g, '').replace(/&#124;/g, '').replace(/&#38;/g, '').replace(/&#63;/g, '').replace(/&#60;/g, '').replace(/&#62;/g, '').replace(/&#61;/g, '').replace(/&#58;/g, '');
+        var sublen = 20;
+        var width = document.body.clientWidth;
+        if (width <= 320) {
+          sublen = 15;
+        }
+        if (width > 320 && width <= 375) {
+          sublen = 18;
+        }
+        if (val.length > sublen) {}
+        return val;
       }
     },
     components: {
